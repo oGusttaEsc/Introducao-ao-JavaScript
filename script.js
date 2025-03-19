a5f3c080e9a490abe2dafd4fbbf8babfad14babf
qrgustta <gustavo_m_silvestre@estudante.sesisenai.org.br>
2025-03-19 09:39:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function verificarPar(numero){
+    if(numero % 2 === 0) {
+        // === é estritamente igual, é igual em tipo de conteudo
+        console.log('par')
+
+    }else{
+        console.log('impar')
+    }
+}
+
+verificarPar(5)
