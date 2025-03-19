0ccafa69727b9f05e47868c3bcdeeded4f0c8798
oGusttaEsc <gustavo_m_silvestre@estudante.sesisenai.org.br>
2025-03-19 10:13:37 -0300

    UPDATE - atividade 3 revisao (contagem / timer de 10 segundos)

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Atividade 1 (verificar par e impar)
+
 function verificarPar(n1){
     if(n1 % 2 === 0) {
         // === é estritamente igual, é igual em tipo de conteudo
@@ -9,6 +11,8 @@ function verificarPar(n1){
 }
 let n1 = parseInt(prompt("Digite um número:"))
 verificarPar(n1)
+
+// Atividade 2 (calculadora)
 
 function calcular(n2, n3){
     const soma = n2 + n3;
@@ -27,3 +31,15 @@ console.log(multiplicacao);
 let n2 = parseFloat(prompt("Digite seu primeiro número:"));
 let n3 = parseFloat(prompt("Digite seu segundo número:"));
 calcular(n2,n3);
+
+// Contagem (timer de 10 segundos)
+
+let n4 = parseInt(prompt("Digite um número entre 10 e 20 para contagem regressiva"));
+
+while (n4 >= 1){
+    setTimeout(()=>{
+        console.log(n4);
+    }, 1000);
+
+
+}
